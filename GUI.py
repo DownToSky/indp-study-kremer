25bc02b8d97e7522a0b36bb46b04b091955359c8
DownToSky <mme94@scarletmail.rutgers.edu>
2020-04-15 15:55:26 -0400

    knobs sync now :)

--- GUI.py
+++ GUI.py
@@ -4,6 +4,7 @@ from PySide2 import QtCore, QtWidgets, QtGui
 from PySide2.QtUiTools import QUiLoader
 import json
 
+
 CONFIG_PATH = "ferret_run.json"
 class UI_Controller(QtWidgets.QMainWindow):
 
@@ -22,26 +23,47 @@ class UI_Controller(QtWidgets.QMainWindow):
         self.resultPath = None
         self.ui.resultPathButton.pressed.connect(self.getResultPath)
 
+        self.ui.doneB.pressed.connect(self.saveConfig)
+
         self.ui.knob1.setMinimum(0)
+        self.ui.knob1.name = "coverage"
         self.ui.knob1.setMaximum(100)
         self.ui.knob1.valueChanged.connect(lambda: self.updatePrefrences(self.ui.knob1))
 
         self.ui.knob2.setMinimum(0)
+        self.ui.knob2.name = "ranking"
         self.ui.knob2.setMaximum(100)
-
+        self.ui.knob2.valueChanged.connect(lambda: self.updatePrefrences(self.ui.knob2))
 
         self.ui.knob3.setMinimum(0)
+        self.ui.knob3.name = "knob 3"
         self.ui.knob3.setMaximum(100)
+        self.ui.knob3.valueChanged.connect(lambda: self.updatePrefrences(self.ui.knob3))
 
         self.configs = None
 
     def updatePrefrences(self, knob):
-        val = self.ui.knob1.value()
-        print("knob change: " + str(val))
+        if self.configs == None:
+            print("load configs first!")
+            return
+            
+        val = knob.value()
+        knob_name = knob.name
+        print(knob_name + " change: " + str(val))
+        if knob.name == "coverage" or knob.name == "ranking":
+            self.configs["preferences"][knob_name] = val
 
+
+    def saveConfig(self):
+        if self.configs == None:
+            print("load configs first!")
+            return
+
+        with open(self.configDir, 'w') as confFile:
+            json.dump(self.configs, confFile)
 
     def loadConfig(self):
-        with open("r", self.configDir) as confFile:
+        with open(self.configDir, 'r') as confFile:
             self.configs = json.load(confFile)
 
     # TODO: makesure the returned path is nonempty
