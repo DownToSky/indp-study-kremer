5d3319452d8fce9e619692e45530ff3c418d8113
Liu Liu <liuliuliu@fb.com>
2020-06-03 17:15:02 -0400

    update result

--- ResultSummarizer.py
+++ ResultSummarizer.py
@@ -13,50 +13,64 @@ def getUsers():
 
 
 def analyseOneFile(file):
-    with open(file) as json_file:
-        result = json.load(json_file)
-        c_list = result['challenge_list']
-        tries = 0.0
-        successes = 0.0
-        overall_quality_achieved = 0.0
-        for c in c_list:
-            target = c['target']
-            tries_per_c = len(c['logs']['tries'])
-            if tries_per_c == 0:  # may have missed this
-                continue
-            tries += tries_per_c
-            qualities = list(map(lambda x: x['quality_percent'], c['logs']['tries']))
-            success = True in list(map(lambda x: x['if_success'], c['logs']['tries']))
-            if success:
-                successes += 1
-                overall_quality_achieved += 1.0
-            else:
-                overall_quality_achieved += max(list(map(lambda x: 1.0 - abs((x - target) / target), qualities)))
+    try:
+        with open(file) as json_file:
+            result = json.load(json_file)
+            c_list = result['challenge_list']
+            tries = 0.0
+            successes = 0.0
+            overall_quality_achieved = 0.0
+            for c in c_list:
+                target = c['target']
+                tries_per_c = len(c['logs']['tries'])
+                if tries_per_c == 0:  # may have missed this
+                    continue
+                tries += tries_per_c
+                qualities = list(map(lambda x: x['quality_percent'], c['logs']['tries']))
+                success = True in list(map(lambda x: x['if_success'], c['logs']['tries']))
+                if success:
+                    successes += 1
+                    overall_quality_achieved += 1.0
+                else:
+                    overall_quality_achieved += max(
+                        list(map(lambda x: 1.0 - abs(((0 if x == -1 else x) - target) / target), qualities)))
 
-        return {'tries': tries / NUM_OF_CHALLENGE, 'success_rate': successes / NUM_OF_CHALLENGE,
-                'quality': overall_quality_achieved / NUM_OF_CHALLENGE}
+            return {'tries': tries / NUM_OF_CHALLENGE, 'success_rate': successes / NUM_OF_CHALLENGE,
+                    'quality': overall_quality_achieved / NUM_OF_CHALLENGE}
+    except Exception as e:
+        print("fail to parse ", file, str(e))
+        return None
+
+
+def summarizeOne(result, resultAll):
+    if result is None:
+        return False
+    for k, v in result.items():
+        if not k in resultAll:
+            resultAll[k] = 0.0
+        resultAll[k] += v
+    return True
 
 
 def scanAll():
     users = getUsers()
     concrete_overall = {}
     virtual_overall = {}
+    tot_virtual = 0.0
+    tot_concrete = 0.0
     for user in users:
         print("scanning:", user)
         concrete_file = BASE_DIR + '/' + user + '/study_logs_concrete.json'
         virtual_file = BASE_DIR + '/' + user + '/study_logs_virtual.json'
         concrete_res = analyseOneFile(concrete_file)
+        if summarizeOne(concrete_res, concrete_overall):
+            tot_concrete += 1
         virtual_res = analyseOneFile(virtual_file)
-        for k, v in concrete_res.items():
-            if not k in concrete_overall:
-                concrete_overall[k] = 0.0
-            concrete_overall[k] += v
-            if not k in virtual_overall:
-                virtual_overall[k] = 0.0
-            virtual_overall[k] += virtual_res[k]
+        if summarizeOne(virtual_res, virtual_overall):
+            tot_virtual += 1
     for k, v in virtual_overall.items():
-        virtual_overall[k] = v / len(users)
-        concrete_overall[k] = concrete_overall[k] / len(users)
+        virtual_overall[k] = v / tot_virtual
+        concrete_overall[k] = concrete_overall[k] / tot_concrete
     print(virtual_overall, concrete_overall)
 
 
